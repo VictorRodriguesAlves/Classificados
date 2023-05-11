ff2e268157e23213c5d9944f695b701056f47e14
Victor <longovictor23@gmail.com>
2023-05-11 16:08:19 -0300

    Iniciado a estilizacao da aba Perfil

--- views/perfil.php
+++ views/perfil.php
@@ -4,20 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/perfilStyle.css">
     <title>Classificados</title>
 </head>
 <body>
-    <h1>Perfil</h1>
-    <img src="<?php echo $_SESSION['foto_usuario'];?>" alt="deu erro" style="width: 30vw;">
-    <p>Ola <?php echo $_SESSION['nome_usuario'];?>, seja bem vindo ao seu perfil!! </p>
-    <a href="home"><input type="submit" value="Voltar"></a>
 
-    <a href="criarAnuncio"><input type="submit" value="Criar anuncio"></a>
+    <div class="header">
+        <div class="esquerda">
+            <h1 class="titulo">Perfil</h1>
+        </div>
+
+        <div class="direita">
+            <h1><a href="criarAnuncio" class="criarAnuncio">Criar anuncio</a></h1>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    <div class="container_foto">
+        <img src="<?php echo $_SESSION['foto_usuario'];?>" alt="imagem de perfil" class="foto_perfil">
+        <h1><?php echo $_SESSION['nome_usuario'];?></h1>
+    </div>
+
+
+    
 
     <?php
         $var = new Anuncio;
     ?>
+
+
+    <div class="parent-element">
+        <a href="home"><div class="arrow left"><div></div></div></a>
+    </div>
+
+
+    <div class="anuncios">
+        <h1>Seus anuncios:</h1>
+
+        <div class="abas">
+            <span>add</span>
+        </div>
+    </div>
 
 </body>
 </html>
